d17cc57e4ababd791be26ded8f24af2e6beb3d52
jlbuild <juliabuildbot@gmail.com>
2026-08-15 09:33:39 +0000

    catch22_jll build 0.6.0+0

--- src/catch22_jll.jl
+++ src/catch22_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("catch22")
-JLLWrappers.@generate_main_file("catch22", UUID("8a07c0c5-99ad-56cb-bc82-72eed1bb61ce"))
+JLLWrappers.@generate_main_file("catch22", Base.UUID("8a07c0c5-99ad-56cb-bc82-72eed1bb61ce"))
 end  # module catch22_jll
